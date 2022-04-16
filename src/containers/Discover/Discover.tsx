bcb72dc8e74ed0ac30d69016f70536396416498e
Daniel D Orlando <dan@danorlando.com>
2022-04-16 10:07:35 -0700

    Add styling to home and discover pages

--- src/containers/Discover/Discover.tsx
+++ src/containers/Discover/Discover.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
+import { PageContainer } from "../../components";
+import styles from "./styles.module.css";
 
 const Discover = () => {
-  return <div>This is the discovery page.</div>;
+  return (
+    <PageContainer>
+      <div className={styles.discoverPageWrapper}>
+        <div className={styles.discoverContainer}>
+          <h1>Discover Things</h1>
+          <p>This is the Discover page.</p>
+        </div>
+      </div>
+    </PageContainer>
+  );
 };
 
 export default Discover;
--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
+import { PageContainer } from "../../components";
+import styles from "./styles.module.css";
 
 const Home = () => {
-  return <div>This is home!</div>;
+  return (
+    <PageContainer>
+      <div className={styles.homeWrapper}>
+        <div className={styles.homeContainer}>
+          <h1>Welcome</h1>
+          <p>This is the home page.</p>
+        </div>
+      </div>
+    </PageContainer>
+  );
 };
 
 export default Home;
